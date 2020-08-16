ddc2357a143d39ecebd5d8261966871444bd7e15
bar.harel <bar.harel@biocatch.com>
2020-08-16 23:00:51 +0300

    added connect func

--- yellowbox/networks.py
+++ yellowbox/networks.py
@@ -1,9 +1,13 @@
 from contextlib import contextmanager
-from typing import Generator, TypeVar
+from typing import Generator, TypeVar, Union
 from uuid import uuid1
 
 from docker import DockerClient
 from docker.models.networks import Network
+from docker.models.containers import Container
+
+from yellowbox.service import YellowService
+
 
 _T = TypeVar("_T")
 _NT = TypeVar("_NT", bound=Network)
@@ -36,6 +40,30 @@ def temp_network(client: DockerClient, name=None, *args, **kwargs):
     with disconnecting(client.networks.create(name, *args, **kwargs),
                        remove=True) as network:
         yield network
+
+
+@contextmanager
+def connect(network: Network, obj: Union[Container, YellowService]):
+    """Temporarily connect a container or yellow service into a network.
+
+    Args:
+        network: Docker network.
+        obj: Container or YellowService to connect to the network.
+
+    Returns:
+        Context manager for handling the connection.
+    """
+    if isinstance(obj, YellowService):
+        obj.connect(network)
+    else:
+        network.connect(obj)
+    try:
+        yield
+    finally:
+        if isinstance(obj, YellowService):
+            obj.disconnect(network)
+        else:
+            network.disconnect(obj)
 
 
 @contextmanager
