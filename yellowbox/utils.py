913043aa0aeadb3c456a4c028b51524a5595aaab
bar.harel <bar.harel@biocatch.com>
2020-08-17 00:43:48 +0300

    Temporarily returned loggingiterableadapter

--- yellowbox/utils.py
+++ yellowbox/utils.py
@@ -1,6 +1,11 @@
+import io
+import json
+import threading
+from collections import deque
+from concurrent.futures.thread import ThreadPoolExecutor
 from contextlib import AbstractContextManager, contextmanager, nullcontext, suppress
 from time import sleep
-from typing import Callable, Iterable, TypeVar, Union
+from typing import Any, Callable, Deque, Dict, Iterable, Iterator, Optional, TypeVar, Union
 
 from yaspin import yaspin
 
@@ -63,3 +68,89 @@ def _get_spinner(fake=False) -> Callable[[str], AbstractContextManager]:
     if fake:
         return lambda text: nullcontext()
     return _spinner
+
+
+class LoggingIterableAdapter(Iterable[Dict[str, Any]]):
+    """An adapter to convert blocking docker logs to non-blocking message streams, not thread-safe
+    """
+
+    # if the iterator takes more than this amount of time, we're gonna say there's no log messages to read
+    nonblocking_timeout = 0.1
+
+    def __init__(self, source: Iterator[bytes]):
+        self.source = source
+        self.pending: Deque[Dict[str, Any]] = deque()
+        self.executor = ThreadPoolExecutor(1)
+        self.done = False
+
+    def _load(self):
+        if self.done:
+            raise StopIteration
+        try:
+            message_bundle = next(self.source)
+        except StopIteration:
+            self.done = True
+            raise
+        else:
+            messages = str(message_bundle, 'utf-8').splitlines()
+            self.pending.extend(
+                json.loads(msg) for msg in messages
+            )
+
+    def read(self):
+        if not self.pending:
+            self._load()
+        return self.pending.popleft()
+
+    def read_nonblocking(self):
+        if self.pending:
+            return self.pending.popleft()
+        self.executor.submit(self._load)
+        sleep(self.nonblocking_timeout)
+        if self.done:
+            raise StopIteration
+        if not self.pending:
+            return None
+        return self.pending.popleft()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.read()
+
+
+# TODO: complete
+class _IterableIO(io.TextIOBase):
+    def __init__(self, iterable: Iterable[str],
+                 buffer_size=io.DEFAULT_BUFFER_SIZE) -> None:
+        self.iterator = iter(iterable)
+        self._buffer = deque()
+        self.buffer_size = buffer_size
+        self._current_buffer_size = 0
+        self._waiter = threading.Event()
+        self._done = False
+        self.blocking = True
+        self._read_thread = threading.Thread(target=self._read_loop)
+
+    def _read_loop(self):
+        while not self.closed:
+            pass
+
+    def setblocking(self, mode: bool) -> None:
+        self.blocking = False
+
+    def readable(self) -> bool:
+        return True
+
+    def read(self, size: Optional[int] = None) -> str:
+        if size == 0:
+            return ""
+
+        if not self.blocking and not self._buffer:
+            exc = BlockingIOError()
+            exc.characters_written = 0
+            raise exc
+
+        if size is None or size < 0:
+            pass
