1b2ff7e85db64f4d6b0d0bf06de917399637c75f
Ben Avrahami <ben.avrahami@biocatch.com>
2020-08-17 13:24:58 +0300

    patch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author="biocatch ltd",
     url="https://github.com/biocatchltd/yellowbox",
     version='0.0.1',
-    packages=['yellowbox', 'yellowbox.fixtures', 'yellowbox.specialized'],
+    packages=['yellowbox', 'yellowbox.extras'],
     python_requires='>=3.7',
     requires=['docker', 'yaspin'],
     extras_require={
