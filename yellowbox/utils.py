6521e3f69bc5ca57b8a8a2c2b518950b8537b976
bar.harel <bar.harel@biocatch.com>
2020-08-18 15:14:32 +0300

    wip

--- yellowbox/utils.py
+++ yellowbox/utils.py
@@ -124,22 +124,56 @@ class LoggingIterableAdapter(Iterable[Dict[str, Any]]):
 # TODO: complete
 class _IterableIO(io.TextIOBase):
     def __init__(self, iterable: Iterable[str],
-                 buffer_size=io.DEFAULT_BUFFER_SIZE) -> None:
+                 buffer_size: int = io.DEFAULT_BUFFER_SIZE) -> None:
+
+        if buffer_size <= 0:
+            raise ValueError("Buffer size must be greater than 0.")
+
         self.iterator = iter(iterable)
         self._buffer = deque()
         self.buffer_size = buffer_size
         self._current_buffer_size = 0
+        self._read_lock = threading.Lock()
+        self._sync_lock = threading.Lock()
         self._waiter = threading.Event()
+        self._resume = threading.Event()
+        self._exc = None
         self._done = False
         self.blocking = True
-        self._read_thread = threading.Thread(target=self._read_loop)
+        self._read_thread = threading.Thread(target=self._read_loop,
+                                             daemon=True)
 
     def _read_loop(self):
-        while not self.closed:
-            pass
+        try:
+            while not self.closed:
+                try:
+                    data = next(self.iterator)
 
-    def setblocking(self, mode: bool) -> None:
-        self.blocking = False
+                    if not isinstance(data, str):
+                        raise TypeError(f"Iterator should return strings, not "
+                                        f"{data.__class__.__name__}.")
+
+                except StopIteration:
+                    break
+
+                with self._sync_lock:
+                    self._buffer.append(data)
+                    self._current_buffer_size += len(data)
+                    if self._current_buffer_size >= self.buffer_size:
+                        self._resume.clear()
+
+                self._waiter.set()
+                self._resume.wait()
+
+        except BaseException as exc:
+            import traceback
+            traceback.clear_frames(exc.__traceback__)
+            self._exc = exc
+
+        finally:
+            self.done = True
+            self._waiter.set()
+
 
     def readable(self) -> bool:
         return True
@@ -149,9 +183,45 @@ class _IterableIO(io.TextIOBase):
             return ""
 
         if not self.blocking and not self._buffer:
-            exc = BlockingIOError()
-            exc.characters_written = 0
-            raise exc
+            raise BlockingIOError()
 
         if size is None or size < 0:
-            pass
+            if self.done:
+                if self._exc is not None:
+                    raise self._exc
+                output = "".join(self._buffer)
+                self._buffer.clear()
+                return output
+
+            if not self.blocking:
+                raise BlockingIOError()
+
+            while not self.done:
+                self._waiter.wait()
+                self._waiter.clear()
+                self._resume.set()
+
+            if self._exc is not None:
+                raise self._exc
+
+            output = "".join(self._buffer)
+            self._buffer.clear()
+            return output
+
+        output = []
+        if size >= self._current_buffer_size:
+            try:
+                item = self._buffer.popleft()
+                while size > len(item):
+                    size -= len(item)
+                    output.append(item)
+                    item = self._buffer.popleft()
+            # No matter what happens here (signals etc.), we need to return the
+            # buffer to a safe state
+            except BaseException:
+                self._current_buffer_size = sum(map(len, self._buffer))
+                raise
+            # Last part
+
+
+
