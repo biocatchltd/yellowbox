05b973bc32fa552b139e5f2a2303210b440fc305
Ben Avrahami <ben.avrahami@biocatch.com>
2020-08-27 16:12:55 +0300

    .

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,3 +1,4 @@
 # run various linters
-python -m poetry run flake8 --max-line-length 120 yellowbox/ tests/
-python -c "import sys; sys.exit(sys.version_info < (3,8,0))" && python -m poetry run pytype --keep-going yellowbox
+python -m poetry run flake8 --max-line-length 120 yellowbox tests
+python -c "import sys; sys.exit(sys.version_info >= (3,8,0))" &&
+ python -m poetry run pytype --keep-going yellowbox
