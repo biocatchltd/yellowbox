dcf87d51103e97746dfa70a964f23c7511690ffe
Ben Avrahami <ben.avrahami@biocatch.com>
2020-08-27 16:07:30 +0300

    no way this works

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,3 +1,3 @@
 # run various linters
 python -m poetry run flake8 --max-line-length 120 yellowbox/ tests/
-python -m poetry run pytype --keep-going yellowbox
+python -c "import sys; sys.exit(sys.version_info < (3,8,0))" && python -m poetry run pytype --keep-going yellowbox
