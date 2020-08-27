4830b9af1af149d9f080b4b7d5b3e7b411a8fe4b
Ben Avrahami <ben.avrahami@biocatch.com>
2020-08-27 16:47:50 +0300

    .

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,11 +1,11 @@
 # run various linters
-flake8 --max-line-length 120 yellowbox tests
-python -c "import sys; sys.exit(sys.version_info < (3,8,0))"
+poetry run flake8 --max-line-length 120 yellowbox tests
+poetry run python -c "import sys; sys.exit(sys.version_info < (3,8,0))"
 res=$?
 if [ "$res" -eq "0" ]
   then
     echo "pytype not run, please run in python 3.7"
   else
-    pytype --keep-going yellowbox
+    poetry run pytype --keep-going yellowbox
 fi
 
