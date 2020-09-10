42d086b290e0a701c911e53439d2ad9894f683db
Ben Avrahami <ben.avrahami@biocatch.com>
2020-09-10 10:37:02 +0300

    added version string (#43)

--- yellowbox/__init__.py
+++ yellowbox/__init__.py
@@ -1,9 +1,11 @@
 from yellowbox.service import YellowService
 from yellowbox.subclasses import ContainerService, SingleEndpointService, SingleContainerService
 from yellowbox.networks import temp_network, connect
+from yellowbox._version import __version__
 
 __all__ = [
     'YellowService',
     'ContainerService', 'SingleEndpointService', 'SingleContainerService',
-    'temp_network', 'connect'
+    'temp_network', 'connect',
+    '__version__'
 ]
