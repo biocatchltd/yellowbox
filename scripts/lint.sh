613a903a95d4509e4e5c9829669ac4324af93a25
ben avrahami <ben.avrahami@biocatch.com>
2021-01-05 14:50:37 +0200

    lint script fix

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # run various linters
+set -e
 poetry run flake8 --max-line-length 120 yellowbox tests
+set +e
 poetry run python -c "import sys; sys.exit(sys.version_info < (3,9,0))"
 res=$?
+set -e
 if [ "$res" -eq "0" ]; then
   echo "pytype not run, please run in python 3.8 or lower"
 else
