9d11ba4d6687630b8abe537adad08eb60d5c9e02
ben avrahami <ben.avrahami@biocatch.com>
2021-01-05 14:43:28 +0200

    .

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 # run various linters
-set -e
 poetry run flake8 --max-line-length 120 yellowbox tests
 poetry run python -c "import sys; sys.exit(sys.version_info < (3,9,0))"
 res=$?
-if [ "$res" -eq "0" ]
-  then
-    echo "pytype not run, please run in python 3.7"
-  else
-    poetry run pytype --keep-going yellowbox
+if [ "$res" -eq "0" ]; then
+  echo "pytype not run, please run in python 3.8 or lower"
+else
+  poetry run pytype --keep-going yellowbox
 fi
-
