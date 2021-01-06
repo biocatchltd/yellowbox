ab459e9f23cdfa85b678052b3bfaf2870241c7fc
ben avrahami <ben.avrahami@biocatch.com>
2021-01-06 16:42:34 +0200

    clear_state

--- tests/extras/test_redis.py
+++ tests/extras/test_redis.py
@@ -17,27 +17,16 @@ def test_connection_works(docker_client):
             assert client.get('a') == b'12'
 
 
-def test_clean_slate(docker_client):
-    with RedisService.run(docker_client) as redis:
-        with redis.client() as client:
-            with redis.clean_slate():
-                client.set('a', 12)
-            assert not client.keys()
-
-
-def test_clean_slate_fail(docker_client):
+def test_reset_state(docker_client):
     with RedisService.run(docker_client) as redis:
         with redis.client() as client:
             client.set('a', 12)
-
-        with raises(RuntimeError, match='.*not empty.*'):
-            with redis.clean_slate():
-                pass
+            redis.reset_state()
+            assert not client.keys()
 
 
 def test_set_state(docker_client):
-    with RedisService.run(docker_client) as redis, \
-            redis.clean_slate():
+    with RedisService.run(docker_client) as redis:
         redis.set_state({
             'a': 12,
             'b': {'i': 0, 'am': 2, 'hungry': 3},
--- yellowbox/extras/redis.py
+++ yellowbox/extras/redis.py
@@ -60,18 +60,9 @@ class RedisService(SingleContainerService, RunMixin):
         self.started = True
         return self
 
-    @contextmanager
-    def clean_slate(self):
-        """
-        Notes:
-            This feature is experimental.
-        """
+    def reset_state(self):
         client: Redis
         with self.client() as client:
-            keys = client.keys()
-            if keys:
-                raise RuntimeError(f'Redis db is not empty (found keys {keys})')
-            yield
             client.flushall()
 
     def set_state(self, db_dict: RedisState):
