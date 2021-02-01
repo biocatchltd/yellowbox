728877c762eea2eb2fe05788856568bddd1e0083
Bar Harel <bzvi7919@gmail.com>
2021-02-01 08:24:43 +0200

    wip

--- yellowbox/extras/websocket.py
+++ yellowbox/extras/websocket.py
@@ -1,36 +1,100 @@
+from __future__ import annotations
+
+from http.server import BaseHTTPRequestHandler
 import re
+from subprocess import Popen
 from unittest.mock import Mock
-from threading import Lock
+from typing import Any, Callable, Generator, Optional, Pattern, List, Dict, Union, cast, no_type_check, overload
+from threading import RLock
 from simple_websocket_server import WebSocket, WebSocketServer
+from functools import partial, wraps
+from types import new_class
+from weakref import WeakMethod
 
 from yellowbox.service import YellowService
 
 
-class WebsocketImpl:
-    service = None
+class _WebsocketTemplate(WebSocket):
+    callback = None
+
+
+@no_type_check
+def _to_generator(side_effect: SIDE_EFFECT_TYPE
+                  ) -> Callable[[BaseHTTPRequestHandler], _GENERAOTR_TYPE]:
+    """Convert a side effect to a generator function.
+
+    Args:
+        side_effect: See WebsocketService.route().
+
+    Returns:
+        Generator function, same as the one in WebsocketService.route().
+    """
+    # Side effect == normal string
+    if isinstance(side_effect, (str, bytes, bytearray, memoryview)):
+        def gen(*args: Any, **kwargs: Any) -> _GENERAOTR_TYPE:
+            yield side_effect
+        return gen
+
+    @wraps(side_effect)  # type: ignore # Mypy GH-10002
+    def gen(*args: Any, **kwargs: Any) -> _GENERAOTR_TYPE:
+        # Side effect == normal function that returns a string.
+        result = side_effect(*args, **kwargs)
+        if isinstance(result, (str, bytes, bytearray, memoryview)):
+            yield result
+        else:
+            # Side effect == generator function
+            return (yield from result)
+
+    return gen
+
+
+_YIELDTYPES = Union[str, bytes, bytearray, memoryview]
+
+_GENERAOTR_TYPE = Generator[_YIELDTYPES,
+                            bytearray, Any]
+
+SIDE_EFFECT_TYPE = Union[
+    _YIELDTYPES, Callable[[BaseHTTPRequestHandler], Optional[_YIELDTYPES]],
+    Callable[[BaseHTTPRequestHandler], _GENERAOTR_TYPE]
+]
 
 
 class WebsocketService(YellowService):
-    def __init__(self):
-        self._routes = {}
-        self._lock = Lock()
-        self._server = a
+    def __init__(self) -> None:
+        self._routes: Dict[str, Callable] = {}
+        self._re_routes: List[Pattern[str]] = []
+        self._lock = RLock()
 
-    def start(self):
-        pass
+        class _WebsocketImpl(_WebsocketTemplate):
+            pass
+        # self._server = a
 
-    def stop(self):
-        pass
+    def start(self) -> None:
+        return super().start()
+
+    def stop(self) -> None:
+        return super().stop()
 
     def _create_websocket(self):
         pass
 
-    def route(self, uri=None, side_effect=None, *, regex=None):
+    def get(self, uri=None, regex=None):
+        """Gets a previously added route.
+
+        """
+
+    def route(self, uri: Optional[str] = None, *,
+              regex: Optional[Union[Pattern[str], str]] = None
+              ) -> Callable[[SIDE_EFFECT_TYPE], None]:
         """Add a route.
+
+        Raises an exception if the route already exists.
 
         Args:
             side_effect: Side effect can be either a string or a
             generator function.
+
+        Raises:
         """
 
         if uri and regex:
@@ -39,21 +103,71 @@ class WebsocketService(YellowService):
         if not (uri or regex):
             raise ValueError("URI or regex must be specified.")
 
-        # Allow decorator syntax
-        if not side_effect:
-            return partial(self.respond, uri=uri, regex=regex)
+        return partial(self.add, uri=uri, regex=regex)
 
-    def remove(self, uri=None, *, regex=None):
+    def add(self, side_effect: SIDE_EFFECT_TYPE,
+            uri: Optional[str] = None, *,
+            regex: Optional[Union[Pattern[str], str]] = None,
+            _overwrite: bool = False) -> None:
+        """Add a route.
+
+        Raises an exception if the route already exists.
+
+        Args:
+            side_effect: Side effect can be either a string or a
+            generator function.
+
+        Raises:
+        """
+
         if uri and regex:
             raise ValueError("Only one of URI or regex can be specified.")
 
         if not (uri or regex):
             raise ValueError("URI or regex must be specified.")
 
-        with self._lock:
-            del self._routes[uri or re.compile(regex)]
+        if regex:
+            regex = re.compile(regex)
 
-    def clear(self):
+        gen = _to_generator(side_effect)
+
+        # Check and place
+        with self._lock:
+            if not _overwrite and (uri in self._routes or
+                                   regex in self._re_routes):
+                raise RuntimeError(f"Route {uri or regex} already exists!")
+
+            if uri:
+                self._routes[uri] = gen
+            else:
+                self._re_routes.append(gen)
+
+    def set(self, side_effect: SIDE_EFFECT_TYPE,
+            uri: Optional[str] = None, *,
+            regex: Optional[Union[Pattern[str], str]] = None) -> None:
+        """Set a route
+
+        Like `route()` but overwrites existing routes.
+        """
+        self.add(side_effect=side_effect, uri=uri,
+                 regex=regex, _overwrite=True)
+
+    def remove(self, uri: Optional[str] = None, *,
+               regex: Optional[Union[Pattern[str], str]] = None) -> None:
+        if uri and regex:
+            raise ValueError("Only one of URI or regex can be specified.")
+
+        if not (uri or regex):
+            raise ValueError("URI or regex must be specified.")
+
+        if uri:
+            del self._routes[uri]
+        else:
+            assert regex  # For Mypy
+            with self._lock:
+                self._re_routes.remove(re.compile(regex))
+
+    def clear(self) -> None:
         """Remove all routes"""
         with self._lock:
             self._routes.clear()
