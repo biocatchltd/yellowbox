6cf1c525aeb5e415c9fbac94ef53b9b9ee24849d
ben avrahami <ben.avrahami@biocatch.com>
2021-07-27 11:52:51 +0300

    .

--- yellowbox/extras/webserver.py
+++ yellowbox/extras/webserver.py
@@ -1,26 +1,69 @@
-from dataclasses import dataclass
-from typing import Dict, List, Optional, Union, Tuple, Callable
+from __future__ import annotations
 
-from flask import Flask, Response
+from abc import ABC, abstractmethod
+from typing import List, Union, Iterable, Optional
 
-HandlerOutput = Union[Response, int, str, bytes, Tuple[int, Union[str, bytes]]]
+from flask import Flask, Response, Request
+from flask.typing import ResponseReturnValue
 
-@dataclass
-class HTTPCall:
-    method: str
-    path: str
-    query_params: Dict[str, List[str]]
-    header: Dict[str, List[str]]
-    body: Optional[bytes]
+HandlerOutput = Union[ResponseReturnValue, int]
 
+class HandlerError(Exception):
+    """
+    An exception occurred while handling an endpoint in the webserver thread
+    """
+    pass
 
-class HTTPEndPoint:
+class ExceptionCell:
+    # since the main thread won't catch errors in handlers, this class will store any error raised while handling,
+    #  and raise them in the main thread as soon as we can
     def __init__(self):
-        self.calls: List[HTTPCall] = []
+        self.exc : Optional[Exception] = None
+
+    def raise_if_error(self):
+        if self.is_error():
+            raise self.exc
+
+    def is_error(self)->bool:
+        return self.exc is not None
+
+class HTTPEndPoint(ABC):
+    owner: WebServer
+
+    def __init__(self, methods: Union[str, Iterable[str]], rule_string: str, **rule_options):
+        self._calls: List[Request] = []
+
+
+        if isinstance(methods, str):
+            methods = (methods,)
+
+        self.methods: Iterable[str] = tuple(methods)
+        self.rule_string = rule_string
+        self.rule_options = rule_options
+
+    @abstractmethod
+    def handle(self, **kwargs) -> HandlerOutput:
+        pass
+
+    def view_func(self, **kwargs):
+        if not hasattr(self, 'owner'):
+            raise RuntimeError('endpoint must be assigned to a webserver')
+        if self.owner.exception_cell.is_error():
+            return f'an exception in the webserver had previously occurred: {self.owner.exception_cell.exc!r}', 500
+        try:
+            ret = self.handle(**kwargs)
+        except Exception as ex:
+            self.owner.exception_cell.exc = ex
+            return f'handler raised an exception: {ex!r}'
+        else:
+
+        return ret
 
 
 class WebServer:
     def __init__(self, name: str, *args, **kwargs):
         self._flask = Flask(name, *args, **kwargs)
+        self.exception_cell = ExceptionCell()
 
-    def add_http_endpoint(self, handler: Callable[..., HandlerOutput], rule_string: str, **rule_options):
+    def add_http_endpoint(self, endpoint: HTTPEndPoint):
+        self._flask.add_url_rule(endpoint.rule_string, methods=endpoint.methods, view_func=endpoint.view_func)
