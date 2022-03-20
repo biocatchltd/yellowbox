d030c92cfd6606656774a6b8ca74e52d1f2aacdb
ben avrahami <ben.avrahami@biocatch.com>
2022-03-20 13:10:31 +0200

    .

--- scripts/unittest.sh
+++ scripts/unittest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # run the unittests with branch coverage
-poetry run coverage run --include="yellowbox/*" -m pytest -n auto --dist loadfile tests/ "$@"
+poetry run coverage run --include="yellowbox/**" -m pytest -n auto --dist loadfile tests/ "$@"
 coverage html
 coverage report -m
 coverage xml
