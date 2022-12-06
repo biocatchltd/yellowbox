e765cfadd633c67803b1f9f606bc051903016774
ben avrahami <ben.avrahami@biocatch.com>
2022-12-06 12:53:24 +0200

    made it easier to monkeypatch the global

--- yellowbox/utils.py
+++ yellowbox/utils.py
@@ -1,7 +1,9 @@
+import os
 import platform
+import sys
 from contextlib import AbstractContextManager, closing, contextmanager, nullcontext
 from socket import AF_INET, SO_REUSEADDR, SOCK_STREAM, SOL_SOCKET, socket
-from typing import Callable, TypeVar
+from typing import Callable, TypeVar, Optional
 
 from yaspin import yaspin
 
@@ -43,14 +45,46 @@ else:
 
 # if we expose a port in docker, this is where we expect to find it hosted
 # this is almost always just localhost
-DOCKER_EXPOSE_HOST = '127.0.0.1'
-if ('microsoft' in uname) and ('wsl2' in uname):
-    # wsl2 exposes ports on the microsoft machine, so we need to target it instead
-    with open('/etc/resolv.conf') as resolv:
-        for line in resolv:
-            if line.startswith("nameserver "):
-                DOCKER_EXPOSE_HOST = line[len("nameserver "):].strip()
-                print(f"resolved DOCKER_EXPOSE_HOST to be {DOCKER_EXPOSE_HOST}")
-                break
-        else:
-            print(f"could not resolve {DOCKER_EXPOSE_HOST}, using default")
+# you can set the value here to some value with the YELLOWBOX_DOCKER_EXPOSE_HOST env-var
+# DO NOT manually change this const, instead call update_docker_expose_host
+DOCKER_EXPOSE_HOST = ""
+
+def update_docker_expose_host(value: Optional[str], print_file):
+    """
+    update the global docker expose host, attempting to infer from the environment
+
+    Args:
+        value: if provided, will use this value instead of inferring a host
+        print_file: the stream to print messages to on inference
+
+    Notes:
+        This function is called once when yellowbox is imported with a value of the env var
+        YELLOWBOX_DOCKER_EXPOSE_HOST
+
+    """
+    global DOCKER_EXPOSE_HOST
+
+    def _helper():
+        if value is not None:
+            return value
+
+        if ('microsoft' in uname) and ('wsl2' in uname):
+            # wsl2 exposes ports on the microsoft machine, so we need to target it instead
+            with open('/etc/resolv.conf') as resolv:
+                for line in resolv:
+                    if line.startswith("nameserver "):
+                        ret = line[len("nameserver "):].strip()
+                        print(
+                            f"resolved DOCKER_EXPOSE_HOST to be {ret}",
+                            file=print_file,
+                        )
+                        return ret
+                else:
+                    print(
+                        f"could not resolve DOCKER_EXPOSE_HOST from resolution file, using default",
+                        file=print_file,
+                    )
+        return "127.0.0.1"
+
+    DOCKER_EXPOSE_HOST = _helper()
+update_docker_expose_host(os.getenv("YELLOWBOX_DOCKER_EXPOSE_HOST"), sys.stderr)
