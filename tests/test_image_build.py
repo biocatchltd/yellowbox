c4b7d1b9ae3ec157083496adce5588f034663254
David Miaskovsky <david.miaskovsky@biocatch.com>
2023-08-13 13:14:27 +0300

    async test

--- tests/test_image_build.py
+++ tests/test_image_build.py
@@ -1,3 +1,5 @@
+from asyncio import gather
+
 from docker.errors import DockerException, ImageNotFound
 from pytest import mark, raises
 
@@ -19,31 +21,32 @@ def test_valid_image_build(docker_client):
 
 @mark.asyncio
 async def test_valid_image_build_async(docker_client):
-    async with async_build_image(
-        docker_client,
-        image_name="yellowbox:test1",
-        path=".",
-        dockerfile="tests/resources/valid_dockerfile/Dockerfile",
-    ) as first_image:
-        async with async_build_image(
+    building_tasks = [
+        async_build_image(
+            docker_client,
+            image_name="yellowbox:test1",
+            path=".",
+            dockerfile="tests/resources/valid_dockerfile/Dockerfile",
+        ),
+        async_build_image(
             docker_client,
             image_name="yellowbox:test2",
             path=".",
             dockerfile="tests/resources/invalid_run_dockerfile/Dockerfile",
-            remove_image=True,
-        ) as second_image:
-            first_container = docker_client.containers.create(first_image)
-            first_container.start()
-            first_container.wait()  # wait for the container to end and close
-            first_container.remove()
+        ),
+    ]
+    images = await gather(*(s.__aenter__() for s in building_tasks))
 
-            # The second_image must not exist because not created
-            with raises(ImageNotFound):
-                docker_client.containers.create(second_image)
-
-    # The first_image must not exist because deleted
+    # existing image
+    container = docker_client.containers.create(images[0])
+    container.start()
+    container.wait()  # wait for the container to end and close
+    container.remove()
+    # unknown image
     with raises(ImageNotFound):
-        docker_client.containers.create("yellowbox:test1")
+        docker_client.containers.create(images[1])
+
+    await gather(*(s.__aexit__(None, None, None) for s in building_tasks))
 
 
 def test_invalid_parse_image_build(docker_client):
