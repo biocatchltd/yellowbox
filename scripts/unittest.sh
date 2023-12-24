519ecff559c025409db668f48abc66dec2e96db8
Ben Avrahami <wouldntyouliketoknow@weather.boy>
2023-12-24 14:32:51 +0200

    profile

--- scripts/unittest.sh
+++ scripts/unittest.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 # run the unittests with branch coverage
-poetry run pytest -n auto --dist loadfile --cov-branch --cov=./yellowbox --cov-report=xml --cov-report=html --cov-report=term-missing tests/ "$@"
+poetry run pytest --durations=50 -n auto --dist loadfile --cov-branch --cov=./yellowbox --cov-report=xml --cov-report=html --cov-report=term-missing tests/ "$@"
