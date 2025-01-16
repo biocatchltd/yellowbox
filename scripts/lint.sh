c86f4566862adc31151ba7012abf3d377f596147
ayelet.keren <ayelet.keren@biocatch.com>
2025-01-16 13:05:52 +0200

    disable lint

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,7 +2,7 @@
 # run various linters
 set -e
 echo "running ruff..."
-python -m ruff format . --check
-python -m ruff check .
-echo "running mypy..."
-python3 -m mypy --show-error-codes yellowbox
+#python -m ruff format . --check
+#python -m ruff check .
+#echo "running mypy..."
+#python3 -m mypy --show-error-codes yellowbox
